4506e59210c690c41ec65e464e626e538b1d4ee3
vanolincikus <vanolcincikuse@gmail.com>
2020-04-02 13:19:41 +0300

    adding functionality

---
+++ src/company/vanolincikus/AcademyBarFrame.java
@@ -0,0 +1,12 @@
+package company.vanolincikus;
+
+import javax.swing.*;
+
+public class AcademyBarFrame  extends JFrame {
+
+    public AcademyBarFrame (){
+
+        
+
+    }
+}
--- src/company/vanolincikus/Main.java
+++ src/company/vanolincikus/Main.java
@@ -3,6 +3,9 @@ package company.vanolincikus;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        AcademyBarFrame frame = new AcademyBarFrame();
+
     }
 }
